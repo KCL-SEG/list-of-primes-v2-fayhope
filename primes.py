27f975e6e40767234372c9aec0f23d724e561e33
fayhope <70073329+fayhope@users.noreply.github.com>
2023-10-16 13:40:08 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
     list = []
+    x = 2
+    if number_of_primes > 0 and isinstance(number_of_primes, int):
+        while len(list) is not number_of_primes:
+            midpoint = x//2
+            prime = True 
+            for y in range(2,midpoint+1):
+                if x%y == 0:
+                    prime = False
+            if prime:
+                list.append(x)
+            x += 1
+            print(list)
+    else:
+        raise ValueError
     return list
